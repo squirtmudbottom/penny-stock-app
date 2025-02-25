785619e6e0cb57972aa5d0aae5ad34dfa2ac4db2
squirtmudbottom <65777413+squirtmudbottom@users.noreply.github.com>
2025-02-25 11:56:12 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 // Adjust the baseURL below to point to your deployed backend
 const baseURL = "plankton-app-ht33g.ondigitalocean.app";
+const API_BASE_URL = "https://plankton-app-ht33g.ondigitalocean.app";
 
 export async function fetchTopStocks() {
   try {
